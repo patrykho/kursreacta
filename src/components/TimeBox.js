25c6596a5cb6d3c446fbaf4ae5161956644d6ce6
Patrick Holody <patryk.holody@gmail.com>
2019-09-17 23:05:16 +0200

    add styles

--- src/components/TimeBox.js
+++ src/components/TimeBox.js
@@ -19,7 +19,7 @@ export const TimeBox = ({ title, totalTimeInMinutes, onDelete, onEdit }) => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
             <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
                     <br></br>
-                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                    <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
                 </div>
             </div>
             <footer className="card-footer">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import bulma from 'bulma'
 import App from './containers/App'
+import "./styles/main.scss"
 
 
 ReactDOM.render(<App />, document.getElementById("root"))
