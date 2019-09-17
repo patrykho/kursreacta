a6dbf497d2a5204799d45915f4edc569d50b3b5f
Patrick Holody <patryk.holody@gmail.com>
2019-09-17 22:59:13 +0200

    App

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { TimeboxList } from './TimeboxList'
+const App = () => {
+    return (<div className="root">
+
+        <TimeboxList />
+    </div>);
+}
+export default App
