8a715ebfea59da5c8a43347a4d9be8d97911803d
Patrick Holody <patryk.holody@gmail.com>
2019-09-17 22:58:29 +0200

    EditableTimebox

---
+++ src/components/EditableTimebox.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { TimeBoxEditor } from './TimeBoxEditor'
+import { CurrentTimeBox } from "./CurrentTimeBox";
+class EditableTimebox extends React.Component {
+    state = {
+        title: "Uczę się skrótów klawiszowych",
+        totalTimeInMinutes: 2,
+        isEditable: true
+    };
+    handleTitle = (e) => {
+        const newTitle = e.target.value;
+        this.setState({
+            title: newTitle
+        });
+    };
+    handleTime = (e) => {
+        const newTime = e.target.value;
+        this.setState({
+            totalTimeInMinutes: newTime
+        });
+    };
+    onConfirm = () => {
+        this.setState({
+            isEditable: !this.state.isEditable
+        });
+    };
+    render() {
+        const { title, totalTimeInMinutes, isEditable } = this.state;
+        const { handleTime, handleTitle, onConfirm } = this;
+        return (<>
+            <TimeBoxEditor onConfirm={onConfirm} handleTime={handleTime} handleTitle={handleTitle} isEditable={isEditable} title={title} totalTimeInMinutes={totalTimeInMinutes} />
+            <CurrentTimeBox title={title} isEditable={isEditable} onConfirm={onConfirm} totalTimeInMinutes={totalTimeInMinutes} />
+        </>);
+    }
+}
