a6a5b2f78b8b748ac456c5b6ce22eb6b936229dc
Patrick Holody <patryk.holody@gmail.com>
2019-09-18 20:13:54 +0200

    change style

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const Clock = ({ className, minutes = 10, seconds = 33 }) => {
-    return <h2 style={{ backgroundColor: "#533003" }} className={`clock ${className}`}>Pozostało {minutes}:{seconds}</h2>;
+    return <h2 className={`clock ${className}`}>Pozostało {minutes}:{seconds}</h2>;
 }
