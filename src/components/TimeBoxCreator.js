413678628de26e9b8a14802704d9af09f302d545
Patrick Holody <patryk.holody@gmail.com>
2019-09-18 18:30:21 +0200

    added edit TimeBox

--- src/components/TimeBoxCreator.js
+++ src/components/TimeBoxCreator.js
@@ -1,19 +1,28 @@
 import React, { useRef } from 'react'
 import uuid from 'uuid'
 
-export const TimeBoxCreator = ({ onCreate }) => {
-
+export const TimeBoxCreator = ({ dataToUpdate, editing, onCreate }) => {
+    // const { indexToUpdate, title, time } = dataToUpdate
     const titleRef = useRef(null)
     const timeRef = useRef(null)
+
+
 
     const handleSumbmit = (e) => {
         e.preventDefault();
         onCreate({ id: uuid.v4(), title: titleRef.current.value, totalTimeInMinutes: timeRef.current.value });
-    };
-    return (<form onSubmit={handleSumbmit} className="time-box-editor">
-        <label >Co robisz? <input type="text" ref={titleRef} /></label>
-        <label >Ile minut? <input type="number" ref={timeRef} /></label>
-        <button>Dodaj timebox </button>
+        titleRef.current.value = ""
+        timeRef.current.value = ""
 
-    </form>);
+    }
+
+    return (<>
+
+        {editing && dataToUpdate ? (<p>{dataToUpdate.title} {dataToUpdate.indexToUpdate} {dataToUpdate.time}</p >) : (<form onSubmit={handleSumbmit} className="time-box-editor">
+            <label >Co robisz? <input type="text" ref={titleRef} /></label>
+            <label >Ile minut? <input type="number" ref={timeRef} /></label>
+            <button>Dodaj timebox </button>
+        </form>)}
+
+    </>)
 }
--- src/containers/TimeboxList.js
+++ src/containers/TimeboxList.js
@@ -2,14 +2,27 @@ import React from 'react'
 import uuid from 'uuid'
 import { TimeBox } from '../components/TimeBox'
 import { TimeBoxCreator } from "../components/TimeBoxCreator";
+import { Modal } from '../components/Modal'
 export class TimeboxList extends React.Component {
     state = {
         timeboxes: [
             { id: uuid.v4(), title: "Ucze sie list", totalTimeInMinutes: 25 },
             { id: uuid.v4(), title: "Ucze sie list 2", totalTimeInMinutes: 20 },
             { id: uuid.v4(), title: "Ucze sie list 3", totalTimeInMinutes: 4 },
-        ]
+        ],
+        editing: false,
+        dataToUpdate: {
+
+        }
     };
+    toggleModal = () => {
+        console.log("toggleModal")
+        this.setState({
+            editing: false
+        })
+
+    }
+
     addTimeBox = (timebox) => {
         this.setState(prevState => {
             const timeboxes = [timebox, ...prevState.timeboxes];
@@ -17,7 +30,7 @@ export class TimeboxList extends React.Component {
         });
     };
     handleCreate = (data) => {
-        console.log("handleCreate");
+
         this.addTimeBox(data);
     };
     removeTimeBox = (indexToRemove) => {
@@ -26,18 +39,32 @@ export class TimeboxList extends React.Component {
             return { timeboxes };
         });
     };
-    updateTimebox = (indexToUpdate, updatedTimebox) => {
-        console.log("indexToUpdate: ", indexToUpdate, " updatedTimebox: ", updatedTimebox);
+    updateTimebox = (indexToUpdate, title, time) => {
+
+        this.setState({
+            editing: true,
+            dataToUpdate: { indexToUpdate, title, time }
+        })
+        const dataToUpdare = {
+            id: indexToUpdate,
+            title,
+            totalTimeInMinutes: time,
+
+        }
+        console.log("indexToUpdate: ", indexToUpdate, " title: ", title, " time ", time);
         this.setState(prevState => {
-            const timeboxes = prevState.timeboxes.map((timebox, index) => index === indexToUpdate ? updatedTimebox : timebox);
+            const timeboxes = prevState.timeboxes.map((timebox, index) => index === indexToUpdate ? dataToUpdare : timebox);
             return { timeboxes };
         });
     };
     render() {
+        const { editing, dataToUpdate } = this.state
         return (<>
             <TimeBoxCreator onCreate={this.handleCreate} />
-
-            {this.state.timeboxes.map((timebox, index) => (<TimeBox onDelete={() => this.removeTimeBox(index)} onEdit={() => this.updateTimebox(index, { ...timebox, title: "nowy tytul" })} key={index} title={timebox.title} totalTimeInMinutes={timebox.totalTimeInMinutes} />))}
+            {editing ? (<Modal updateTimebox={this.updateTimebox} toggleModal={this.toggleModal} dataToUpdate={dataToUpdate} />) : ("")}
+            {this.state.timeboxes.map((timebox, index) => (<TimeBox onDelete={() => this.removeTimeBox(index)}
+                onEdit={() => this.updateTimebox(index, timebox.title, timebox.totalTimeInMinutes)}
+                key={index} title={timebox.title} totalTimeInMinutes={timebox.totalTimeInMinutes} />))}
 
 
         </>);
