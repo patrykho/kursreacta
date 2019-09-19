32b1f87d8d6f743ea0edb96c14c03fb0a66df5fe
Patrick Holody <patryk.holody@gmail.com>
2019-09-19 19:47:02 +0200

    added classnames

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import classN from 'classnames'
+
 export const ProgressBar = ({ className, percent }) => {
-    return (<div className={`progress-bar ${className}`}>
+    const progressClassName = classN(
+        "progress-bar",
+        className
+    )
+
+    return (<div className={progressClassName}>
         <div style={{ width: `${percent}%` }}></div>
     </div>);
 }
