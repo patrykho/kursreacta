f78a96a8677070f4ae425675ed503fe6bd2c43fa
Patrick Holody <patryk.holody@gmail.com>
2019-09-24 00:05:44 +0200

    done ProgressBar

--- src/components/ProgressBar.test.tsx
+++ src/components/ProgressBar.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import renderer from 'react-test-renderer';
 
 import {ProgressBar} from './ProgressBar';
 
+var clockRenderer: any = null;
 var root: any = null;
+
 describe('<ProgressBar>', () => {
   describe('Given classname and percent', () => {
     beforeEach(() => {
@@ -32,6 +34,26 @@ describe('<ProgressBar>', () => {
     });
     it('then renders classNames clock inactive ', () => {
       expect(root.childNodes[0].className).toMatch(/progress-bar/);
+    });
+  });
+
+  describe('Given className and percent (TestRenderer)', () => {
+    beforeEach(() => {
+      clockRenderer = renderer.create(
+        <ProgressBar className={'inactive'} percent={90} />,
+      );
+    });
+    it('then renders div', () => {
+      expect(clockRenderer.toJSON().type).toEqual('div');
+    });
+    it('then renders classNames progress-bar inactive', () => {
+      expect(clockRenderer.toJSON().props).toMatchObject({
+        className: expect.stringMatching(/progress-bar inactive/),
+      });
+    });
+
+    it('then renders snapshot Clock ', () => {
+      expect(clockRenderer.toJSON()).toMatchSnapshot();
     });
   });
 });
