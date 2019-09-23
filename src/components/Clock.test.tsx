0dafc6d901991b6a7094739095c457befc32b4c2
Patrick Holody <patryk.holody@gmail.com>
2019-09-23 22:19:05 +0200

    Clock tests

--- src/components/Clock.test.tsx
+++ src/components/Clock.test.tsx
@@ -5,14 +5,26 @@ import {Clock} from './Clock';
 
 var root: any = null;
 describe('<Clock', () => {
-  beforeEach(() => {
-    root = document.createElement('div');
-    ReactDOM.render(
-      <Clock className={'inactive'} minutes={10} seconds={15} />,
-      root,
-    );
-  });
-  it('renders properly when given minutes and seconds', () => {
-    expect(root.childNodes[0].nodeName).toEqual('H2');
+  describe('Given minutes and seconds', () => {
+    beforeEach(() => {
+      root = document.createElement('div');
+      ReactDOM.render(
+        <Clock className={'inactive'} minutes={10} seconds={16} />,
+        root,
+      );
+    });
+    it('then renders H2', () => {
+      expect(root.childNodes[0].nodeName).toEqual('H2');
+    });
+    it('then renders classNames clock inactive ', () => {
+      expect(root.childNodes[0].className).toMatch(/clock inactive/);
+    });
+    it('then renders time 10:16 ', () => {
+      expect(root.childNodes[0].textContent).toMatch(/10:16/);
+    });
+
+    it('then renders Pozostało', () => {
+      expect(root.childNodes[0].textContent).toMatch(/Pozostało/);
+    });
   });
 });
